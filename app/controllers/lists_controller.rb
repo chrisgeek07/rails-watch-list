656d3b12982e8d37fee13a77d38fa5628464e70b
Christian BeauxYeux <gildweek@gmail.com>
2021-10-28 23:35:41 +0400

    Added and created review controller with actions, errors to be fixed

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,8 +17,9 @@ class ListsController < ApplicationController
   end
 
   def show
-    @bookmark = Bookmark.new
     @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+    @review = Review.new(list: @list)
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
   resources :lists do
-    resources :bookmarks
+    resources :bookmarks, only: %i[new create]
+    resources :review, only: :create
   end
   resources :bookmarks, only: :destroy
 end
