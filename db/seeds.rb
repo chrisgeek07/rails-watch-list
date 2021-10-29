7783263c0fda0b52fbecb78e9396d4b73f0195c2
Christian BeauxYeux <gildweek@gmail.com>
2021-10-29 12:01:29 +0400

    Fixed links, added styles and created new seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+puts "Cleaning database"
+
+
+puts "Previous data all destroyed and now creating new data."
+
 Movie.create(
   title: 'Wonder Woman 1984',
   overview: 'Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s',
@@ -11,7 +16,7 @@ Movie.create(
 )
 
 Movie.create(
-  title: 'Titanic', 
+  title: 'Titanic',
   overview: '101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.', 
   poster_url: 'https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg', rating: 7.9
 )
@@ -21,3 +26,29 @@ Movie.create(
   overview: 'Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.',
   poster_url: 'https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg', rating: 7.0
 )
+
+Movie.create(
+  title: 'Pulp Fiction',
+  overview: 'A burger-loving hit man, his philosophical partner, a drug-addled gangster\'s moll and a washed-up boxer converge in this sprawling, comedic crime caper. Their adventures unfurl in three stories that ingeniously trip back and forth in time.',
+  poster_url: 'https://image.tmdb.org/t/p/original/d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg', rating: 8.5
+)
+
+Movie.create(
+  title: 'The Green Mile',
+  overview: "A supernatural tale set on death row in a Southern prison, where gentle giant John Coffey possesses the mysterious power to heal people's ailments. When the cell block's head guard, Paul Edgecomb, recognizes Coffey's miraculous gift, he tries desperately to help stave off the condemned man's execution.",
+  poster_url: 'https://image.tmdb.org/t/p/original/velWPhVMQeQKcxggNEU8YmIo52R.jpg', rating: 8.5
+)
+
+Movie.create(
+  title: 'The Godfather',
+  overview: "Spanning the years 1945 to 1955, a chronicle of the fictional Italian-American Corleone crime family. When organized crime family patriarch, Vito Corleone barely survives an attempt on his life, his youngest son, Michael steps in to take care of the would-be killers, launching a campaign of bloody revenge.", 
+  poster_url: 'https://image.tmdb.org/t/p/original/eEslKSwcqmiNS6va24Pbxf2UKmJ.jpg', rating: 8.7
+)
+
+Movie.create(
+  title: "Schindler's List",
+  overview: "The true story of how businessman Oskar Schindler saved over a thousand Jewish lives from the Nazis while they worked as slaves in his factory during World War II.",
+  poster_url: "https://image.tmdb.org/t/p/original/sF1U4EUQS8YHUYjNl3pMGNIQyr0.jpg", rating: 8.6
+)
+
+puts "All movies have been created !"
